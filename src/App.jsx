d0246e5413fb7512a3bf4aa39655ebeeeaec776b
rwam1 <rwam1.nikhil.ub@gmail.com>
2025-04-15 11:11:23 +0530

    add text in app

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum quasi quas atque! {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
